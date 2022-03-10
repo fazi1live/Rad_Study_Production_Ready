64bb0047f4bc0a90c548b7763275c8a7a7f16d33
fazi1live <faziislive@gmail.com>
2022-03-10 20:42:41 +0500

    add

--- configuration/DatabaseConfiguration.js
+++ configuration/DatabaseConfiguration.js
@@ -1,7 +1,7 @@
 const Package = require('../package.json');
 const mongoose = require('mongoose');
 
-const DatabaseName = 'LMSUpworkProject';
+
 mongoose.connect(process.env.DATABASE_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (error,connection) => {
     if (!error) {
         console.log(`\nMogoDb Connected Successfuly at MongoAtlas with Database Name ${DatabaseName}\n`);
