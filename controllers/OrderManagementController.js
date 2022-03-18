49f4b35fdcd4e1d4c6e4c670d01547e12acfab73
fazi1live <faziislive@gmail.com>
2022-03-18 12:40:21 +0500

    add

--- controllers/OrderManagementController.js
+++ controllers/OrderManagementController.js
@@ -1,18 +1,66 @@
+const _OrderModel = require('../models/PlaceOrderManagementModel');
 
-
-const PlaceOrder = async(req, res) => {
+const PlaceOrder = async (req, res) => {
     try {
-        console.log(req.body);
+        const {
+            ExamPlan,
+            ExamPrice,
+            TaxPrice,
+            TotalPricePaid,
+            TotalQuestions,
+            UserEmail,
+            UserName,
+            FirstName,
+            LastName,
+            Country,
+            StreetName,
+            StateProvince,
+            City,
+            Zip,
+            PaidAmountByStudent,
+            Token,
+            PaymentMethod,
+            StudentCountry,
+            PaymentBy,
+            CustomerId,
+            CustomerName
+        } = req.body;
+
+        const OrderToSave = new _OrderModel({
+            ExamPlan: ExamPlan,
+            ExamPrice: ExamPrice,
+            TaxPrice: TaxPrice,
+            TotalPricePaid: TotalPricePaid,
+            TotalQuestions: TotalQuestions,
+            UserEmail: UserEmail,
+            UserName: UserName,
+            FirstName: FirstName,
+            LastName: LastName,
+            Country: Country,
+            StreetName: StreetName,
+            StateProvince: StateProvince,
+            City: City,
+            Zip: Zip,
+            PaidAmountByStudent: PaidAmountByStudent,
+            Token: Token,
+            PaymentMethod: PaymentMethod,
+            StudentCountry: StudentCountry,
+            PaymentBy: PaymentBy,
+            CustomerId: CustomerId,
+            CustomerName: CustomerName
+        });
+
+        const DataToSave = await OrderToSave.save();
         res.json({
-            Message:'Got it',
-            Data:true,
-            Result:true
+            Message: 'Data Has Saved Successfuly',
+            Data: true,
+            Result: true
         })
     } catch (error) {
         req.json({
-            Message:error.message,
-            Data:false,
-            Result:null
+            Message: error.message,
+            Data: false,
+            Result: null
         })
     }
 }
@@ -20,3 +68,6 @@ const PlaceOrder = async(req, res) => {
 module.exports = {
     PlaceOrder
 }
+
+
+
