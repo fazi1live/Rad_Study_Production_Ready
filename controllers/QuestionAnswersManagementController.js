6e9ee31ce62237c0ff01ce4073e7e303d44e724d
fazi1live <faziislive@gmail.com>
2022-03-31 07:22:32 +0500

    add

--- controllers/QuestionAnswersManagementController.js
+++ controllers/QuestionAnswersManagementController.js
@@ -1,10 +1,10 @@
-const QuestionAnswersManagementCluster  = require('../models/QuestionsAnswerManagement');
+const _QuestionAnswersManagementCluster  = require('../models/QuestionsAnswerManagement');
 
 const AddQuestionsForExams = async (req, res) => {
     try {
         const Questions = req.body;
         Questions.forEach(async (KeyPair) => {
-            const DocToSave = new QuestionAnswersManagementCluster({
+            const DocToSave = new _QuestionAnswersManagementCluster({
                 Questions:KeyPair
             })
             await DocToSave.save();
@@ -23,6 +23,24 @@ const AddQuestionsForExams = async (req, res) => {
     }
 }
 
+const GetAllQuestionsAnswers = async (req, res) => {
+    try {
+        const GetAllDocs = await _QuestionAnswersManagementCluster.find().lean();
+        res.json({
+            Message:'Data Found Successfuly',
+            Data:true,
+            Result:GetAllDocs
+        })
+    } catch (error) {
+        res.json({
+            Message:error.message,
+            Data:false,
+            Result:null
+        })
+    }
+}
+
 module.exports = {
-    AddQuestionsForExams
+    AddQuestionsForExams,
+    GetAllQuestionsAnswers
 }
--- routes/QuestionAnswersManagementRoute.js
+++ routes/QuestionAnswersManagementRoute.js
@@ -2,10 +2,12 @@ const express = require('express');
 const Router = express.Router();
 
 const { 
-    AddQuestionsForExams
+    AddQuestionsForExams,
+    GetAllQuestionsAnswers
 } = require('../controllers/QuestionAnswersManagementController');
 
 Router.post('/AddQuestionsForExams',AddQuestionsForExams);
+Router.get('/GetAllQuestionsAnswers',GetAllQuestionsAnswers);
 
 
 module.exports = Router;
